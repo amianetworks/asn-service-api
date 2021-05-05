41672a3b1e6fe911cf9c7be61a8e423fd4d730e5
Fangjia Li <hollandLinus@gmail.com>
2021-05-04 19:46:48 -0700

    add helper function to Version

--- common/version.go
+++ common/version.go
@@ -12,27 +12,37 @@ type Version struct {
 	Build string
 }
 
-func InitVersion(versionStr string) (*Version, error) {
+func InitVersion(versionStr string) (Version, error) {
 	versionParts := strings.Split(versionStr, ".")
 	if len(versionParts) == 3 {
-		return nil, fmt.Errorf("invalid version format, must be 'v.MAJOR.MINOR.BUILD', for example: v1.0.1-20210501")
+		return Version{}, fmt.Errorf("invalid version format, must be 'v.MAJOR.MINOR.BUILD', for example: v1.0.1-20210501")
 	}
 	major, err := strconv.ParseUint(versionParts[0], 10, 664)
 	if err != nil {
-		return nil, fmt.Errorf("invalid major version: %s", versionParts[0])
+		return Version{}, fmt.Errorf("invalid major version: %s", versionParts[0])
 	}
 	minor, err := strconv.ParseUint(versionParts[1], 10, 664)
 	if err != nil {
-		return nil, fmt.Errorf("invalid minor version: %s", versionParts[1])
+		return Version{}, fmt.Errorf("invalid minor version: %s", versionParts[1])
 	}
 
-	return &Version{
+	return Version{
 		Major: major,
 		Minor: minor,
 		Build: versionParts[2],
 	}, nil
 }
 
-func (v *Version) ToString() string {
+func (v Version) ToString() string {
 	return fmt.Sprintf("v%d.%d.%s", v.Major, v.Minor, v.Build)
+}
+
+func (v Version) GreaterAndEqualThan(target Version) bool {
+	if v.Major > target.Major {
+		return true
+	} else if v.Major == target.Major && v.Minor >= target.Minor {
+		return true
+	} else {
+		return false
+	}
 }
