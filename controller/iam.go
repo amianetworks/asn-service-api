7a1ccc14a374b181de02800f909796a3305b87f8
Terry Du <terrydurunkai@gmail.com>
2024-12-18 10:44:37 -0800

    Refactor IAM interface method signatures for consistency.
    Updated method signatures in the IAM interface to fix parameter mismatches, adjust return types, and improve clarity. Updated `mfaCode` types to `int32` and corrected inconsistent parameter lists in several methods. These changes enhance code readability and maintainability.

--- controller/iam.go
+++ controller/iam.go
@@ -3,18 +3,18 @@
 package capi
 
 type IAM interface {
-	AccountCreate(username string, password []byte, info map[string]string) (err error)             // AccountCreate allows the user system to create a new account.
-	AccountRemove(username string) (err error)                                                      // AccountRemove allows the user system to remove an existed account by username.
-	AccountList(filter map[string]string) (userList []string, err error)                            // AccountList allows the user system to query the existed account list.
-	AccountListReverse(filter map[string]string) (userList []string, err error)                     // AccountListReverse allows the user system to query the existed account list.
-	AccountSelfDefineSearch(filter, reverseFilter map[string]string) (userList []string, err error) // AccountSelfDefineSearch allows the user system to query the existed account list.
-	AccountInfoQuery(username string, info []string) (infoMap map[string]string, err error)         // AccountInfoQuery allows the user system to query information of an existed account.
-	AccountInfosQuery(username string, info []string) (infoMap map[string][]string, err error)      // AccountInfosQuery allows the user system to query information of an existed account.
-	AccountInfoUpdate(username string, info map[string]string) (err error)                          // AccountInfoUpdate allows the user system to update information of an existed account.
-	AccountPasswordUpdate(username string, oldPassword, newPassword []byte) (err error)             // AccountPasswordUpdate allows the user system to update password of an existed account.
-	AccountPasswordReset(username string, newPassword []byte) (err error)                           // AccountPasswordReset allows the user system to reset password of an existed account.
-	AccountRecoverEmailSend(username string) (err error)                                            // AccountRecoverEmailSend allows the user system to send a verify code to user email.
-	AccountRecoverByEmail(username string, newPassword []byte, code string) (err error)             // AccountRecoverByEmail allows the user system to recover account by email.
+	AccountCreate(username string, password []byte, info map[string]string) (err error)        // AccountCreate allows the user system to create a new account.
+	AccountRemove(username string) (err error)                                                 // AccountRemove allows the user system to remove an existed account by username.
+	AccountList(filter map[string]string) (userList []string, err error)                       // AccountList allows the user system to query the existed account list.
+	AccountListReverse(filter, reverseFilter map[string]string) (userList []string, err error) // AccountListReverse allows the user system to query the existed account list.
+	AccountSelfDefineSearch(filter string) (userList []string, err error)                      // AccountSelfDefineSearch allows the user system to query the existed account list.
+	AccountInfoQuery(username string, info []string) (infoMap map[string]string, err error)    // AccountInfoQuery allows the user system to query information of an existed account.
+	AccountInfosQuery(username string, info []string) (infoMap map[string][]string, err error) // AccountInfosQuery allows the user system to query information of an existed account.
+	AccountInfoUpdate(username string, info map[string]string) (err error)                     // AccountInfoUpdate allows the user system to update information of an existed account.
+	AccountPasswordUpdate(username string, oldPassword, newPassword []byte) (err error)        // AccountPasswordUpdate allows the user system to update password of an existed account.
+	AccountPasswordReset(username string, newPassword []byte) (err error)                      // AccountPasswordReset allows the user system to reset password of an existed account.
+	AccountRecoverEmailSend(username string) (err error)                                       // AccountRecoverEmailSend allows the user system to send a verify code to user email.
+	AccountRecoverByEmail(username string, newPassword []byte, code string) (err error)        // AccountRecoverByEmail allows the user system to recover account by email.
 
 	UserGroupAdd(sysType, groupName string) (groupID string, err error)                              // UserGroupAdd allows the user system to add a new user group.
 	UserGroupDelete(sysType, groupID string) (err error)                                             // UserGroupDelete allows the user system to delete a new user group.
@@ -24,19 +24,19 @@ type IAM interface {
 	UserJoinUserGroup(sysType string, username []string, userGroupID string) (err error)             // UserJoinUserGroup allows the user system to bind a account to a user group.
 	UserLeaveUserGroup(sysType string, username []string, userGroupID string) (err error)            // UserLeaveUserGroup allows the user system to unbind a account from a user group.
 
-	Login(sysType, username string, password []byte, entity *EntityBaseInfo) (needMFAVerify bool, qrImg, secret, accessToken, refreshToken string) // Login allows the user system to  login.
-	PasswordVerify(username string, password []byte) (err error)                                                                                   // PasswordVerify allows the user system to check password of user.
-	MFAVerify(sysType, username string, mfaCode int, mfaType string) (err error)                                                                   // MFAVerify allows the user system to perform Multi-Factor Authentication.
-	MFALoginVerify(sysType, username string, mfaCode int, mfaType string) (accessToken, refreshToken string, err error)                            // MFALoginVerify allows the user system to perform Multi-Factor Authentication while login.
-	AuthenticatorBindConfirm(sysType, username string, mfaCode int, secret string) (err error)                                                     // AuthenticatorBindConfirm allows the user system to perform Multi-Factor Authentication.
-	AuthenticatorBind(sysType, username string, mfaCode int, secret string) (err error)                                                            // AuthenticatorBind allows the user to bind when Authenticator is needed.
-	AuthenticatorBindStatus(sysType, username string) (bind bool, err error)                                                                       // AuthenticatorBindStatus allows the user to bind when Authenticator is needed.
-	AuthenticatorUnbind(sysType, username string) (err error)                                                                                      // AuthenticatorUnbind allows the user to unbind when Authenticator isn't needed or rebind.
-	EntityList(sysType, username string) (info map[string]*EntityBaseInfo, err error)                                                              // EntityList allows the admin to get all bound entities of target user.
-	EntityNameUpdate(sysType, username, entityID, entityName string) (err error)                                                                   // EntityNameUpdate allows the admin to rename entity.
-	EntityAdd(sysType, username string, info *EntityBaseInfo) (err error)                                                                          // EntityAdd allows the admin to add bound entities of target user.
-	EntityDelete(sysType, username, entityID string) (err error)                                                                                   // EntityDelete allows the admin to delete bound entities of target user.
-	Logout(sysType, username string) (err error)                                                                                                   // Logout allows the user logout and redirect to login page.
+	Login(sysType, username string, password []byte, entity *EntityBaseInfo) (needMFAVerify bool, qrImg, secret, accessToken, refreshToken string, err error) // Login allows the user system to  login.
+	PasswordVerify(username string, password []byte) (err error)                                                                                              // PasswordVerify allows the user system to check password of user.
+	MFAVerify(sysType, username string, mfaCode int32, mfaType string) (err error)                                                                            // MFAVerify allows the user system to perform Multi-Factor Authentication.
+	MFALoginVerify(sysType, username string, mfaCode int32, mfaType string) (accessToken, refreshToken string, err error)                                     // MFALoginVerify allows the user system to perform Multi-Factor Authentication while login.
+	AuthenticatorBindConfirm(sysType, username string, mfaCode int32, secret string) (err error)                                                              // AuthenticatorBindConfirm allows the user system to perform Multi-Factor Authentication.
+	AuthenticatorBind(sysType, username string) (err error)                                                                                                   // AuthenticatorBind allows the user to bind when Authenticator is needed.
+	AuthenticatorBindStatus(sysType, username string) (bind bool, err error)                                                                                  // AuthenticatorBindStatus allows the user to bind when Authenticator is needed.
+	AuthenticatorUnbind(sysType, username string) (err error)                                                                                                 // AuthenticatorUnbind allows the user to unbind when Authenticator isn't needed or rebind.
+	EntityList(sysType, username string) (info map[string]*EntityBaseInfo, err error)                                                                         // EntityList allows the admin to get all bound entities of target user.
+	EntityNameUpdate(sysType, username, entityID, entityName string) (err error)                                                                              // EntityNameUpdate allows the admin to rename entity.
+	EntityAdd(sysType, username string, info *EntityBaseInfo) (err error)                                                                                     // EntityAdd allows the admin to add bound entities of target user.
+	EntityDelete(sysType, username, entityID string) (err error)                                                                                              // EntityDelete allows the admin to delete bound entities of target user.
+	Logout(sysType, username string) (err error)                                                                                                              // Logout allows the user logout and redirect to login page.
 
 	TokenRefresh(sysType, username, deviceCode, refreshToken, accessToken string) (newAccessToken string, err error)                                  // TokenRefresh allows the user system to refresh access token.
 	TokenVerify(sysType, username, deviceCode, accessToken string) (err error)                                                                        // TokenVerify allows the user system to verify valid of access token.
