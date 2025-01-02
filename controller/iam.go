ced4d66b8c2f1693f87280b1130653c87e78f1af
Terry Du <terrydurunkai@gmail.com>
2025-01-02 14:08:36 -0800

    Update IAM interface with userClaims and refine token methods
    Revised the IAM interface to include userClaims in relevant methods for enhanced security and flexibility. Simplified token verification and added token revocation functionality by updating method signatures. Removed the unused EntityBaseInfo struct to streamline the codebase.

--- controller/iam.go
+++ controller/iam.go
@@ -24,22 +24,19 @@ type IAM interface {
 	UserJoinUserGroup(sysType string, username []string, userGroupID string) (err error)             // UserJoinUserGroup allows the user system to bind a account to a user group.
 	UserLeaveUserGroup(sysType string, username []string, userGroupID string) (err error)            // UserLeaveUserGroup allows the user system to unbind a account from a user group.
 
-	Login(sysType, username string, password []byte, entity *EntityBaseInfo) (needMFAVerify bool, qrImg, secret, accessToken, refreshToken string, err error) // Login allows the user system to  login.
-	PasswordVerify(username string, password []byte) (err error)                                                                                              // PasswordVerify allows the user system to check password of user.
-	MFAVerify(sysType, username string, mfaCode int32, mfaType string) (err error)                                                                            // MFAVerify allows the user system to perform Multi-Factor Authentication.
-	MFALoginVerify(sysType, username string, mfaCode int32, mfaType string) (accessToken, refreshToken string, err error)                                     // MFALoginVerify allows the user system to perform Multi-Factor Authentication while login.
-	AuthenticatorBindConfirm(sysType, username string, mfaCode int32, secret string) (err error)                                                              // AuthenticatorBindConfirm allows the user system to perform Multi-Factor Authentication.
-	AuthenticatorBind(sysType, username string) (qrImg, secret string, err error)                                                                             // AuthenticatorBind allows the user to bind when Authenticator is needed.
-	AuthenticatorBindStatus(sysType, username string) (bind bool, err error)                                                                                  // AuthenticatorBindStatus allows the user to bind when Authenticator is needed.
-	AuthenticatorUnbind(sysType, username string) (err error)                                                                                                 // AuthenticatorUnbind allows the user to unbind when Authenticator isn't needed or rebind.
-	EntityList(sysType, username string) (info map[string]*EntityBaseInfo, err error)                                                                         // EntityList allows the admin to get all bound entities of target user.
-	EntityNameUpdate(sysType, username, entityID, entityName string) (err error)                                                                              // EntityNameUpdate allows the admin to rename entity.
-	EntityAdd(sysType, username string, info *EntityBaseInfo) (err error)                                                                                     // EntityAdd allows the admin to add bound entities of target user.
-	EntityDelete(sysType, username, entityID string) (err error)                                                                                              // EntityDelete allows the admin to delete bound entities of target user.
-	Logout(sysType, username string) (err error)                                                                                                              // Logout allows the user logout and redirect to login page.
+	Login(sysType, username string, password []byte, userClaims string) (needMFAVerify bool, qrImg, secret, accessToken, refreshToken string, err error) // Login allows the user system to  login.
+	PasswordVerify(username string, password []byte) (err error)                                                                                         // PasswordVerify allows the user system to check password of user.
+	MFAVerify(sysType, username string, mfaCode int32, mfaType string) (err error)                                                                       // MFAVerify allows the user system to perform Multi-Factor Authentication.
+	MFALoginVerify(sysType, username string, mfaCode int32, mfaType, userClaims string) (accessToken, refreshToken string, err error)                    // MFALoginVerify allows the user system to perform Multi-Factor Authentication while login.
+	AuthenticatorBindConfirm(sysType, username string, mfaCode int32, secret string) (err error)                                                         // AuthenticatorBindConfirm allows the user system to perform Multi-Factor Authentication.
+	AuthenticatorBind(sysType, username string) (qrImg, secret string, err error)                                                                        // AuthenticatorBind allows the user to bind when Authenticator is needed.
+	AuthenticatorBindStatus(sysType, username string) (bind bool, err error)                                                                             // AuthenticatorBindStatus allows the user to bind when Authenticator is needed.
+	AuthenticatorUnbind(sysType, username string) (err error)                                                                                            // AuthenticatorUnbind allows the user to unbind when Authenticator isn't needed or rebind.
+	Logout(sysType, username string) (err error)                                                                                                         // Logout allows the user logout and redirect to login page.
 
-	TokenRefresh(sysType, username, deviceCode, refreshToken, accessToken string) (newAccessToken string, err error)                                  // TokenRefresh allows the user system to refresh access token.
-	TokenVerify(sysType, username, deviceCode, accessToken string) (err error)                                                                        // TokenVerify allows the user system to verify valid of access token.
+	TokenRefresh(sysType, userClaims, refreshToken, accessToken string) (newAccessToken string, err error)                                            // TokenRefresh allows the user system to refresh access token.
+	TokenVerify(sysType, accessToken string) (err error)                                                                                              // TokenVerify allows the user system to verify valid of access token.
+	TokenRevoke(sysType, accessToken string) (err error)                                                                                              // TokenRefresh allows the user system to revoke access token.
 	RoleAdd(sysType, roleName, remark string) (err error)                                                                                             // RoleAdd allows the user system to add a new role.
 	RoleDelete(sysType, roleName string) (err error)                                                                                                  // RoleDelete allows the user system to delete a role.
 	RoleList(sysType string, filter map[string]string) (roleList map[string]string, err error)                                                        // RoleList allows the user system to query the existed roleList.
@@ -62,11 +59,4 @@ type IAM interface {
 	UserPermissionQuery(sysType, username string) (userPermission []string, inheritedFromUserGroup, inheritedFromRole map[string][]string, err error) // UserPermissionQuery allows the user system to query identity and access control of a user.
 	UserGroupPermissionQuery(sysType, userGroupID string) (userGroupPermission []string, inheritedFromRole map[string][]string, err error)            // UserGroupPermissionQuery allows the user system to query identity and access control of a user group.
 	RolePermissionQuery(sysType, roleName string) (rolePermission []string, err error)                                                                // RolePermissionQuery allows the user system to query identity and access control of a role.
-}
-
-type EntityBaseInfo struct {
-	ID   string
-	Name string
-	Type string
-	OS   string
 }
