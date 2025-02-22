18494f08cca72054e63998d07b538dcf3089e521
Terry Du <terrydurunkai@gmail.com>
2025-02-22 09:26:12 -0800

    Update ApplyServiceOps to use separate error channels
    Refactored the ApplyServiceOps method to use distinct channels for framework and service errors. This improves error handling by allowing more granular error reporting and separation of concerns.

--- servicenode/plugin.go
+++ servicenode/plugin.go
@@ -53,7 +53,7 @@ type ASNService interface {
 		Caution: the service node will have a timeout context (20s) to process the initialization,
 				 if it cannot be done within 20s, service node will assign the state MALFUNCTIONAL to the service
 	*/
-	ApplyServiceOps(opCmd, opParams string, c chan error)
+	ApplyServiceOps(opCmd, opParams string, errChanFramework, errChanService chan error)
 
 	/*
 		Stop the service with the configuration.
