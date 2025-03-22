416edaf8ba8a8cf007b6de0d37606e22ed0906cc
Yunsong Lu <yunsong@amiasys.com>
2025-03-21 17:50:04 -0700

    zzz

--- servicenode/asn.go
+++ servicenode/asn.go
@@ -6,27 +6,34 @@ import (
 	"github.com/amianetworks/asn-service-api/v25/log"
 )
 
-// Structs used for service node and service communication
-
+// ASNServiceNode contains the APIs provided by ASN Service Node.
+//
+// 1. Initialization
+// 2. SendMessageToController
+// 3. ??
+//
 // ASNServiceNode provided by ASN Service Node for Service uses
 type ASNServiceNode interface {
+	//
+	// Initialization
+	// 
+	// InitLogger returns a logger dedicated to the service.
+	// ASN Framework manages logging for all services, and the default log files are <servicename>-*.log
+	InitLogger() (*log.Logger, error)
+
+	// InitDB may be called to initialize multiple DBs with specified dbName and dbType.
+	// The required DB is connected and ready for use through the DBhandle.
+	InitDB(dbType string, dbName string) (dbh DBhandle, error)
+
+	// Placeholder for Locker, in case it's needed.
+	// Placeholder for IAM, in case it's needed.
+
+	//
+	// FIXME: remove such functions.
 	// GetServiceNodeNetif returns ASN managed netifs from Service node.
-	GetServiceNodeNetif() (Netif, error)
+	//GetServiceNodeNetif() (Netif, error)
 
-	// SendMessageToController sends the message to the controller.
-	SendMessageToController(message string) error
-
-	// GetLogger returns the logger for this service.
-	GetLogger() (*log.Logger, error)
-
-	// GetServiceNodeType returns the ASN Service Node type, currently support 'server', 'appliance'.
-	GetServiceNodeType() string
-}
-
-// Netif is the structure for network interface.
-type Netif struct {
-	Data       []string
-	Control    []string
-	Management []string
-	Other      []string
+	// Service Node may send a formated message to its controller, which must have implemented
+	// HandleMessageFromServiceNode() to handle the received message.
+	SendMessageToController(msg []byte) error
 }
