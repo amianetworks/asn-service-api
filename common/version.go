0ce9c5375fbb4ee980ed570402e78cc52812c201
Terry Du <terrydurunkai@gmail.com>
2025-07-24 09:37:20 -0700

    Refactor `Version` struct and related methods to support `Build` struct, improve validation, and standardize format handling

--- common/version.go
+++ common/version.go
@@ -4,6 +4,7 @@ package commonapi
 
 import (
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -12,39 +13,59 @@ const (
 	VersionCompareGreater = 1 + iota
 	VersionCompareLess
 	VersionCompareEqual
+
+	versionFormat = `^v\d+\.\d+(?:\.\d+)?(?:-\d+)?$`
 )
 
 type Version struct {
 	Major uint64
 	Minor uint64
 
-	HasBuild bool
-	Build    string
+	Build *Build
+}
+
+type Build struct {
+	Number uint64
+	Suffix uint64
 }
 
 // InitVersion converts a version string, e.g., "v25.0.1", into Version structure.
 func InitVersion(versionStr string) (Version, error) {
-	versionParts := strings.Split(versionStr, ".")
-	if len(versionParts) != 2 && len(versionParts) != 3 {
-		return Version{}, fmt.Errorf("invalid version format, must be 'v.MAJOR.MINOR' or 'v.MAJOR.MINOR.BUILD', for example: v1.0 or v1.0.1-196912")
+	matched, err := regexp.MatchString(versionFormat, versionStr)
+	if err != nil {
+		return Version{}, err
+	}
+	if !matched {
+		return Version{}, fmt.Errorf("invalid version format, must be 'v.MAJOR.MINOR' or 'v.MAJOR.MINOR.BUILD', for example: v1.0, v1.0.1 or v1.0.1-196912")
 	}
 
-	major, err := strconv.ParseUint(strings.Replace(versionParts[0], "v", "", -1), 10, 64)
-	if err != nil {
-		return Version{}, fmt.Errorf("invalid major version [%s] due to: %v", versionParts[0], err)
-	}
-	minor, err := strconv.ParseUint(versionParts[1], 10, 64)
-	if err != nil {
-		return Version{}, fmt.Errorf("invalid minor version [%s]  due to: %v", versionParts[1], err)
+	versionParts := strings.Split(strings.TrimPrefix(versionStr, "v"), ".")
+
+	major, _ := strconv.ParseUint(versionParts[0], 10, 64)
+	minor, _ := strconv.ParseUint(versionParts[1], 10, 64)
+
+	var build *Build
+	if len(versionParts) == 3 {
+		var buildNum, buildSuffix uint64
+
+		if strings.Contains(versionParts[2], "-") {
+			buildParts := strings.Split(versionParts[2], "-")
+			buildNum, _ = strconv.ParseUint(buildParts[0], 10, 64)
+			buildSuffix, _ = strconv.ParseUint(buildParts[1], 10, 64)
+		} else {
+			buildNum, _ = strconv.ParseUint(versionParts[2], 10, 64)
+		}
+
+		build = &Build{
+			Number: buildNum,
+			Suffix: buildSuffix,
+		}
 	}
 
 	version := Version{
 		Major: major,
 		Minor: minor,
-	}
-	if len(versionParts) == 3 {
-		version.HasBuild = true
-		version.Build = versionParts[2]
+		Build: build,
 	}
 
 	return version, nil
@@ -52,7 +73,15 @@ func InitVersion(versionStr string) (Version, error) {
 
 // ToString converts a Version structure back to the version string.
 func (v Version) ToString() string {
-	return fmt.Sprintf("v%d.%d.%s", v.Major, v.Minor, v.Build)
+	if v.Build == nil {
+		return fmt.Sprintf("v%d.%d", v.Major, v.Minor)
+	}
+
+	if v.Build.Suffix == 0 {
+		return fmt.Sprintf("v%d.%d.%d", v.Major, v.Minor, v.Build.Number)
+	}
+
+	return fmt.Sprintf("v%d.%d.%d-%d", v.Major, v.Minor, v.Build.Number, v.Build.Suffix)
 }
 
 func (v Version) Compare(target Version) int {
@@ -71,17 +100,23 @@ func (v Version) Compare(target Version) int {
 	}
 
 	// same major and minor, compare build
-	if v.HasBuild && target.HasBuild {
-		if v.Build > target.Build {
+	if v.Build != nil && target.Build != nil {
+		if v.Build.Number > target.Build.Number {
 			return VersionCompareGreater
-		} else if v.Build < target.Build {
+		} else if v.Build.Number < target.Build.Number {
 			return VersionCompareLess
 		} else {
-			return VersionCompareEqual
+			if v.Build.Suffix > target.Build.Suffix {
+				return VersionCompareGreater
+			} else if v.Build.Suffix < target.Build.Suffix {
+				return VersionCompareLess
+			} else {
+				return VersionCompareEqual
+			}
 		}
-	} else if v.HasBuild {
+	} else if v.Build != nil {
 		return VersionCompareGreater
-	} else if target.HasBuild {
+	} else if target.Build != nil {
 		return VersionCompareLess
 	} else {
 		return VersionCompareEqual
