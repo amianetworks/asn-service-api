fb36ee4694cd9c23f5904eb6ad5c6dbfec943394
Terry Du <terrydurunkai@gmail.com>
2025-08-07 09:22:37 -0700

    Refactor `Start` and `ApplyServiceOps` method signatures in `servicenode` to streamline return types and improve consistency.

--- servicenode/service.go
+++ servicenode/service.go
@@ -21,14 +21,14 @@ type ASNService interface {
 	// 1. Config:
 	// 	 the configuration of the service. Service MUST update this configuration to the local file.
 	// 	 When DumpConfiguration called, the service needs to return the current configuration to framework
-	// 2. The return value to channel indicate service state:
+	// 2. The return value indicate service state:
 	// 	 - if error is nil, the service node will assign the state CONFIGURED to the service
 	// 	 - if error is NOT nil, the service node will try to init the service and reapply the configuration for 3 times,
 	// 	   after all retry if it is still having error, will assign the state MALFUNCTIONAL to the service
 	//
 	// Caution: the service node will have a timeout context (20s) to process the initialization,
 	//   		if it cannot be done within 20s, service node will assign the state MALFUNCTIONAL to the service
-	Start(config []byte) (errChan <-chan error, err error)
+	Start(config []byte) (response string, err error)
 
 	// UpdateConfig passes a set of configurations to the service.
 	//
@@ -46,14 +46,14 @@ type ASNService interface {
 	// Service operations will not change the service status (enabled/disabled),
 	// but will do some runtime operations such as: insert/delete/getXXX/setXXX
 	// Apply the configuration to the service, this method will be called under a go routine,
-	// the return value to channel indicate service state:
+	// the return value indicate service state:
 	// if error is nil, the service node will remain the previous state (CONFIGURED/INITIALIZED)
 	// if error is NOT nil, the service node will try to init the service and re-apply the configuration for 3 times,
 	// 	 after all retry if it is still having error, will assign the state MALFUNCTIONAL to the service
 	//
 	// Caution: the service node will have a timeout context (20s) to process the initialization,
 	// 		 	if it cannot be done within 20s, service node will assign the state MALFUNCTIONAL to the service
-	ApplyServiceOps(opCmd, opParams string) (response <-chan *commonapi.Response)
+	ApplyServiceOps(opCmd, opParams string) (response string, err error)
 
 	// Stop the service with the configuration.
 	//
